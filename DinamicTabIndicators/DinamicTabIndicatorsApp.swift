2d0635c103999904e4c91850378142b317ce0819
Glauber Gustavo <glauber.filho@madeinweb.app>
2023-02-07 21:21:24 -0300

    Initial Commit

---
+++ DinamicTabIndicators/DinamicTabIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinamicTabIndicatorsApp.swift
+//  DinamicTabIndicators
+//
+//  Created by Glauber Gustavo on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DinamicTabIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
